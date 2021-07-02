d48df0ad89fd7044a6a9d4ea4b161bd67cac2495
Mirim <Mirim@192.168.123.106>
2021-07-02 17:06:09 +0900

    210702 박스오피스 불러오기 완ㅋ /장

---
+++ src/movie/action/MovieBoxOfficeAction.java
@@ -0,0 +1,36 @@
+package movie.action;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import action.Action;
+import api.BoxofficeApi;
+import vo.ActionForward;
+
+public class MovieBoxOfficeAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		String targetDt = request.getParameter("targetDt");
+		
+		BoxofficeApi ba = new BoxofficeApi();
+		String getBox = ba.getBoxOffice(targetDt);
+		
+		JsonParser jsonParser = new JsonParser();
+		JsonObject jsonObject = (JsonObject) jsonParser.parse(getBox);
+		
+		System.out.println("BoxOfficeAction");
+		response.setContentType("application/json;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println(jsonObject);
+		
+		return null;
+	}
+
+}
--- src/movie/dao/MovieDao.java
+++ src/movie/dao/MovieDao.java
@@ -1,6 +1,11 @@
 package movie.dao;
 
+import static db.JdbcUtil.close;
+
 import java.sql.*;
+import java.util.ArrayList;
+
+import movie.vo.MovieBean;
 
 public class MovieDao {
 private MovieDao() {};
@@ -32,4 +37,41 @@ private MovieDao() {};
 		return 0;
 	}
 
+
+
+
+	public ArrayList<MovieBean> getMovieByChart() {
+		
+		System.out.println("dao-chart");
+		ArrayList<MovieBean> list = new ArrayList<MovieBean>();
+		String sql = "SELECT movieseq,title,sum(count),poster, count from chart where date >= date_sub(now(),interval 1 DAY) group by title order by sum(count) desc limit 0,10";
+		try {
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				MovieBean mb = new MovieBean();
+				mb.setMovieSeq(rs.getInt("movieseq"));
+				mb.setMovieTitle(rs.getString("title"));
+				mb.setMoviePoster(rs.getString("poster"));
+				list.add(mb);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rs);
+			close(pstmt);
+		}
+		return list;
+	}
+
 }
+
+
+
+
+
+
+
+
